47a9e616ebf8073909b97411d12e9a5792beaf2a
Krystian Spiewak <krystianspiewak@gmail.com>
2021-05-28 12:34:21 -0500

    courseinfo step3 completed

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -9,10 +9,11 @@ const Header = (props) => {
 };
 
 const Part = (props) => {
+  console.log(props)
   return (
     <div>
       <p>
-        {props.part.name} {props.part.exercise}
+        {props.part.name} {props.part.exercises}
       </p>
     </div>
   )
@@ -36,19 +37,25 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[{name: part1, exercise: exercises1}, {name: part2, exercise: exercises2}, {name: part3, exercise: exercises3}]} />
-      <Total totalCount={exercises1 + exercises2 + exercises3} />
+      <Content parts={[part1, part2, part3]} />
+      <Total totalCount={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
