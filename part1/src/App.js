3a4622187f4a8b36e45a77edd0f2cbf84ebd1495
Krystian Spiewak <krystianspiewak@gmail.com>
2021-05-28 15:18:14 -0500

    part1 example2 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age;
+
   return (
     <div>
-      <p>Hello { props.name }, you are { props.age } years old.</p>
+      <p>
+        Hello {name}, you are {age} years old.
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   );
 };
 
 const App = () => {
-  const name = "Peter";
+  const name = 'Peter';
   const age = 10;
 
   return (
     <div>
-      <p>Greetings</p>
-      <Hello name="George" />
-      <Hello name="Daisy" />
-      <Hello name="Maya" age={ 26 + 10 } />
-      <Hello name={ name } age={ age } />
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   );
 };
