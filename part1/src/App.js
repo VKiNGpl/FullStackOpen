f8312c4961cd96a00ff100e97bb6ba6f4b15b906
Krystian Spiewak <krystianspiewak@gmail.com>
2021-05-28 16:18:29 -0500

    example2 completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import { Hello } from './Hello.js';
 
+const Display = ({ counter }) => <p>{counter}</p>;
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
   const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
 
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name='Maya' age={26 + 10} />
-      <p>{counter}</p>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button text='plus' handleClick={increaseByOne} />
+      <Button text='minus' handleClick={decreaseByOne} />
+      <Button text='zero' handleClick={setToZero} />
     </div>
   );
 };
