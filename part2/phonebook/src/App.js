2e32e512404b05999ad838c7ec8fc13a0bbaf4c2
Krystian Spiewak <krystianspiewak@gmail.com>
2021-07-10 19:56:15 -0500

    2.7: phonebook step2 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [ newName, setNewName ] = useState('')
+  const [newName, setNewName] = useState('');
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const names = persons.map(({ name }) => name);
+    
+    if (names.indexOf(newName) >= 0) {
+      window.alert(`${newName} already exists in phonebook!`);
+      return;
+    }
+
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPerson));
+    setNewName('');
+  };
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNewName} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => (
+          <div key={person.name}>{person.name}</div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
