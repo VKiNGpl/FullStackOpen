a4b80f718dd7a4847575bb7a1d5d9ca3851e259c
Krystian Spiewak <krystianspiewak@gmail.com>
2022-03-13 23:29:12 -0400

    Begin Part 2c

--- part2/src/App.js
+++ part2/src/App.js
@@ -28,12 +28,15 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+      important: Math.random() < 0.5
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data));
+      setNewNote('');
+    })
   };
 
   const notesToShow = showAll
